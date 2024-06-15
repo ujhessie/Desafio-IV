ecd567a8dbd4486dffef7784b9b675c8989e49db
ujhessie <juniorrodrigues4502@gmail.com>
2024-06-15 13:12:32 -0300

    sada

--- src/pages/HomePage/components/Sec_Inicio/Sec_Inicio.jsx
+++ src/pages/HomePage/components/Sec_Inicio/Sec_Inicio.jsx
@@ -22,10 +22,36 @@ export const Sec_Inicio = () => {
 
             <div className='div-botao'>
               <a href='#' className='botao'>
-                
                 <p>CADASTRE-SE</p>
               </a>
             </div>
+
+            <div className='logins'>
+              <a href='#' className='google'>
+                ENTRAR COM GOOGLE
+              </a>
+              <a href='#' className='facebook'>
+                ENTRAR COM FACEBOOK
+              </a>
+            </div>
+
+            <h2 className='logo-2'>Voluntarygame [Logo]</h2>
+
+            <p className='text'>
+              Embarque em uma jornada transformadora com &apos;Desafios
+              Humanitários&apos;. Este jogo inovador coloca você no coração das
+              operações das ONGs, enfrentando os desafios reais que essas
+              organizações encontram diariamente. Desde a mobilização de
+              voluntários até a arrecadação de fundos, cada nível do jogo revela
+              as complexidades e as recompensas do trabalho humanitário. Através
+              de uma narrativa envolvente e mecânicas de jogo realistas,
+              &apos;Desafios Humanitários&apos; não só entretem, mas também
+              educa e inspira. Descubra como suas ações podem fazer a diferença
+              no mundo real e apoie as causas que importam. Junte-se a nós e
+              veja como é ser um herói do cotidiano!
+            </p>
+
+            <p>[botão aqui]</p>
           </div>
         </MaxContainer>
       </section>
