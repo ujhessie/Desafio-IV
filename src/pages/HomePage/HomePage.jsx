304615b05a8ac4a301c9437cda8bf0229f55a23b
ujhessie <juniorrodrigues4502@gmail.com>
2024-06-15 11:48:18 -0300

    asda

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,10 +1,8 @@
-import { Header } from "./../../components/Header/Header";
 import { Sec_Inicio } from "./components/Sec_Inicio/Sec_Inicio";
 
 export const HomePage = () => {
   return (
     <div>
-      <Header />
       <Sec_Inicio />
     </div>
   );
--- src/pages/HomePage/components/Sec_Inicio/Sec_Inicio.jsx
+++ src/pages/HomePage/components/Sec_Inicio/Sec_Inicio.jsx
@@ -1,16 +1,33 @@
-import "./sec_Inicio.css"
+import { Header } from "../../../../components/Header/Header";
+import "./sec_Inicio.css";
+import { MaxContainer } from "./../../../../components/MaxContainer/MaxContainer";
 
 export const Sec_Inicio = () => {
   return (
     <>
-      <section>
-        <h1>Seção de Inicio</h1>
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci id
-          aperiam quod officiis, ullam consequatur blanditiis ad! Ipsa nostrum,
-          dolorem molestias neque ab quisquam voluptatem, ad incidunt veritatis,
-          veniam repellendus?
-        </p>
+      <Header />
+      <section id='hero-section'>
+        <MaxContainer>
+          <div className='content-sec-inicio'>
+            <h1>Desvende os Desafios das ONGs e Faça a Diferença.</h1>
+            <p className='subtitulo'>
+              Um jogo transformador que coloca você no centro das ações
+              humanitárias.
+            </p>
+            <div className='ctas'>
+              <div>Jogue</div>
+              <div>Entenda</div>
+              <div>Apoie</div>
+            </div>
+
+            <div className='div-botao'>
+              <a href='#' className='botao'>
+                
+                <p>CADASTRE-SE</p>
+              </a>
+            </div>
+          </div>
+        </MaxContainer>
       </section>
     </>
   );
